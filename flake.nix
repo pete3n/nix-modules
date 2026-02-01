bf4050c2626f9596c2e7eba7ce28e3dbf489691c
pete3n <pete3n@protonmail.com>
2026-02-01 14:27:42 -0500

    fix: linux homeManager modules

--- flake.nix
+++ flake.nix
@@ -3,39 +3,42 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
-  outputs = { nixpkgs, ... }:
-  let
-    lib = nixpkgs.lib;
-    importModule = path: import path;
-  in
-  {
-    nixosModules = {
-      power-management = {
-        lidmond = importModule ./nixos/power-management/lidmond.nix;
-      };
-
-			# Hardware specific modules
-      hardware = {
-        framework16 = {
-          kbd-alsd = importModule ./nixos/hardware/framework16/fw16-kbd-alsd.nix;
+  outputs =
+    { nixpkgs, ... }:
+    let
+      lib = nixpkgs.lib;
+      importModule = path: import path;
+    in
+    {
+      nixosModules = {
+        power-management = {
+          lidmond = importModule ./nixos/power-management/lidmond.nix;
         };
+
+        # Hardware specific modules
+        hardware = {
+          framework16 = {
+            kbd-alsd = importModule ./nixos/hardware/framework16/fw16-kbd-alsd.nix;
+          };
+        };
+
+        # Default merge all generic modules
+        default = lib.mkMerge [
+          (importModule ./nixos/power-management/lidmond.nix)
+        ];
       };
 
-			# Default merge all generic modules
-      default = lib.mkMerge [
-        (importModule ./nixos/power-management/lidmond.nix)
-      ];
-    };
+      homeManagerModules = {
+        linux = {
+          power-management = {
+            hyprlidmon = importModule ./home-manager/linux/power-management/hyprlidmon.nix;
+          };
+        };
 
-    homeManagerModules = {
-      power-management = {
-        hyprlidmon = importModule ./home-manager/linux/power-management/hyprlidmon.nix;
+        # Default merge all generic modules
+        default = lib.mkMerge [
+          (importModule ./home-manager/linux/power-management/hyprlidmon.nix)
+        ];
       };
-
-			# Default merge all generic modules
-      default = lib.mkMerge [
-        (importModule ./home-manager/linux/power-management/hyprlidmon.nix)
-      ];
     };
-  };
 }
